089b1f576365495071eed3e925e011ba1d179cf3
Blue <13604413+Blumlaut@users.noreply.github.com>
2020-11-05 20:31:30 +0100

    Update chatra.user.js

--- chatra/chatra.user.js
+++ chatra/chatra.user.js
@@ -12,7 +12,7 @@
 // @grant              GM_getValue
 // @grant              GM_setValue
 // @run-at        document-start
-// @version 13
+// @version 14
 // @updateURL https://raw.githubusercontent.com/Blumlaut/userstyles-tamperscripts/main/chatra/chatra.user.js
 // @downloadURL https://raw.githubusercontent.com/Blumlaut/userstyles-tamperscripts/main/chatra/chatra.user.js
 // ==/UserScript==
